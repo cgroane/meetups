88bd390918d97bf8f8ce363e403ef491837c5548
Connor Roane <connorroane@Connors-MacBook-Pro.local>
2018-03-28 16:22:44 -0500

    prep for hosting

--- server/index.js
+++ server/index.js
@@ -8,12 +8,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static(`${__dirname}/../build`))
+
 const groupCtrl = require('./controllers/groupInfoController');
 
 app.get('/api/react-group', groupCtrl.getReactJSInfo);
 app.get('/api/react-group/:eventID', groupCtrl.getRSVPs);
 app.post('/api/places', groupCtrl.getPlaces);
 app.get('/api/places/:place', groupCtrl.getPlaceDetails);
+
+const path = require('path');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../build/index.html'));
+})
 
 const port = process.env.PORT || 3001;
 
